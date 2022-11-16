cad1893214aa3452a600d348860ee1bc362385bd
Salih Çakmak <slhckmk01@gmail.com>
2022-11-17 01:06:42 +0300

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by ALFA on 17.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
